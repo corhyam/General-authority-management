58cb62ec4e066112b9dc00777cb257ce2fc061d7
gd.pang <gd.pang@foxmail.com>
2020-05-30 14:29:39 +0800

    修改人：庞志权 修改原因：添加用户管理 修改时间：2020/5/30

--- src/main/java/com/jier/admin/dao/UserMapper.java
+++ src/main/java/com/jier/admin/dao/UserMapper.java
@@ -27,4 +27,8 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    List<User> selectAllUser();
+
+    int delUserByID(List<String> ids);
 }
---
+++ src/main/java/com/jier/admin/service/UserService.java
@@ -0,0 +1,15 @@
+package com.jier.admin.service;
+
+import com.github.pagehelper.PageInfo;
+import com.jier.admin.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public interface UserService{
+    PageInfo<User> selectAllUser(int page, int limit);
+    int delUserByID(List<String> ids);
+    int insertSelective(User record);
+}
